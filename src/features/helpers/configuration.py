c446bbbe115aed3b89de4a6a49b364b7ea016a61
Eliot Jones <elioty@hotmail.co.uk>
2015-12-03 23:12:22 +0000

    choose chromedriver based on platform

--- src/features/helpers/configuration.py
+++ src/features/helpers/configuration.py
@@ -1,8 +1,13 @@
 import configparser
 import os
+import sys
+import platform
 from .browsers import Browsers
 
 projectRoot = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+
+def is_64bit():
+    return sys.maxsize > 2**32
 
 def getConfig():
     configFilePath = os.path.join(projectRoot, "src", "config.ini")
@@ -19,7 +24,16 @@ def get_browser():
     return Browsers.get_browser(get_setting("selenium", "driver"))
     
 def read_chromedriver_location():
-    return os.path.join(projectRoot, "tools", "chromedriver.exe")
+    return os.path.join(projectRoot, "tools", get_chromedriver())
 
 def read_internetexplorer_location():
     return os.path.join(projectRoot, "tools", "IEDriverServer.exe")
+    
+def is_windows():
+    return platform.system().lower() == "windows"
+    
+def is_linux():
+    return platform.system().lower() == "linux"
+    
+def get_chromedriver():
+    return "chromedriver.exe" if is_windows else "chromedriver64" if is_64bit() else "chromedriver"
