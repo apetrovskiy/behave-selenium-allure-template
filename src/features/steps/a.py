1e218624e61d70d5cb642017ce976162457fe02c
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2021-11-08 12:52:04 +0300

    Update a.py

--- src/features/steps/a.py
+++ src/features/steps/a.py
@@ -1,7 +1,9 @@
 from behave import given, step
+from behave.runner import Context
 
 
+@step('step 0001')
 @given(u'ccc')
-def step_impl(context):
+def step_impl(context: Context):
     # raise NotImplementedError(u'STEP: Given ccc')
     pass
